eb5b3d9f53769f0b4407cba634732ee553671a18
Johandre Delgado <johanesweb@gmail.com>
2020-07-22 11:32:45 +0300

    Extract row UI into separate subview

--- Combiny/ContentView.swift
+++ Combiny/ContentView.swift
@@ -15,33 +15,9 @@ struct ContentView: View {
     var body: some View {
       NavigationView {
         List(dataManager.posts) { post in
-          ZStack {
-            Rectangle()
-              .foregroundColor(Color(UIColor.systemGray6))
-            VStack {
-              HStack {
-                Text(String(post.title.first ?? Character("X")).uppercased())
-                .font(.largeTitle)
-                .foregroundColor(.white)
-                .padding()
-                .background(Circle()
-                    .fill(Color.red)
-                    .frame(width: 60, height: 60)
-                )
-                VStack(alignment: .leading) {
-                  Text(post.title.prefix(15))
-                    .font(.headline)
-                  Text("User \(post.id)")
-                    .foregroundColor(Color(UIColor.systemGray2))
-                }
-                .padding(.horizontal)
-                Spacer()
-              }
-              Text(post.body)
-            }
-          .padding()
-          }
-        }.onAppear {
+          PostRow(post: post)
+        }
+        .onAppear {
           self.dataManager.loadStores()
           print("onAppear: \(self.dataManager.posts.count)")
         }
@@ -55,3 +31,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
---
+++ Combiny/Views/PostRow.swift
@@ -0,0 +1,53 @@
+//
+//  PostRow.swift
+//  Combiny
+//
+//  Created by Johandre Delgado  on 22.07.2020.
+//  Copyright © 2020 Johandre Delgado . All rights reserved.
+//
+
+import SwiftUI
+
+struct PostRow: View {
+  let post: SimplePost
+
+  var body: some View {
+    ZStack {
+      Rectangle()
+        .foregroundColor(Color(UIColor.systemGray6))
+
+      VStack {
+        HStack {
+          Text(String(post.title.first ?? Character("X")).uppercased())
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .background(
+              Circle()
+                .fill(Color.red)
+                .frame(width: 60, height: 60)
+          )
+
+          VStack(alignment: .leading) {
+            Text(post.title.prefix(15))
+              .font(.headline)
+            Text("User \(post.id)")
+              .foregroundColor(Color(UIColor.systemGray2))
+          }
+          .padding(.horizontal)
+
+          Spacer()
+        }
+
+        Text(post.body)
+      }
+      .padding()
+    }
+  }
+}
+
+struct PostRow_Previews: PreviewProvider {
+  static var previews: some View {
+    PostRow(post: SimplePost(id: 1, title: "Hola mis amigos feos", body: "Esto de Combine mola bien chido y chévere parcero"))
+  }
+}
